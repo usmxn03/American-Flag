7204b5ee2278f860507b7a2bf4c2f057b62ae5b1
Syed Usman Hussain <116179527+usmxn03@users.noreply.github.com>
2023-01-26 13:15:14 +0000

    Add files via upload

---
+++ Americanflag.java
@@ -0,0 +1,24 @@
+package americanflag;
+
+// This code was created by Usman 2152992
+
+//Start of program
+
+public class Americanflag {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 6; x++){
+        System.out.println("******=================================") ;
+        
+        // Print the american flag with the stars and the stripes using * and =
+        
+        } 
+        for (int y = 1; y <= 4; y++){
+        System.out.println("======================================="); 
+        
+         // Print the american flag with only the stripes using =
+         
+        } 
+    }  
+}
+
+//End of program
